5ef4a822051a339c87dea554f983d019063477d7
JessacaSummers <26254063+JessacaSummers@users.noreply.github.com>
2018-05-26 18:12:45 -0500

    Added mass import
    Added ability to mass-import cards by entering them manually into a modal. Also cleaned up some extraneous code and files.

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,39 +1,123 @@
+angular.module('ygo_folder_app', []);
 
-/*$( document ).ready(function() {
+angular.module('ygo_folder_app', []).controller('ygo_folder_controller', function ygo_folder_controller($scope) {
     
-    $(".btn-outline-secondary").on('click', function(){
-	//var name='lunalight blue cat';
-	getCard(search_card);
-    });
-
-});*/
-
-var ygo_folder_app = angular.module('ygo_folder_app', []);
-
-ygo_folder_app.controller('ygo_folder_controller', function ygo_folder_controller($scope) {
+    /* 
+     *      Variable Initializations 
+     */
     
-    var c = this;
-    
-    c.search_list = [];
-    c.collection = [];
-
-    c.search_for_card = function(card){
+    //Define this controller as a variable for simplicity of handling scope
+    c = this;
         
-        getCard(card);
-        //console.log(c.search_list);
+    //Declare array that will be used when searching
+    c.search_list = {};
+    c.search_list.cards = [];
+    
+    //Declare string that will be parsed when importing cards into a collection
+    c.import_cards = ""; 
+    
+    //Declare int that will keep track of which collection in collections is "active"
+    c.current_collection = -1;
+    
+    //Initializes the collections
+    if (true){ //If no existing data
+        c.collections = [{
+            "name": "Main Collection",
+            "cards": []
+        }];
+    
+        c.current_collection = 0;
+    }    
+    
+    /*
+     *       Generic
+     */
+    c.process_card_data = function(data){
+        var card = JSON.parse(data);
+        return card.card;
+    };
+
+    /*
+     *       Searching
+     */
+
+    //Validates user input, then analyses their entry to see if it matches
+    //any card names. Then, sends that name to an AJAX call to handle accordingly
+    c.search_for_card = function(entry){
+        get_card_from_api(entry, c.search_list);
+        //refresh();
+        console.log(c.search_list);
     };
     
-    c.add_to_collection = function(card){
-        console.log("add_to_collection was called");
-        c.collection.push(card);
-        console.log(c.collection);
+    function refresh(){
+        $scope.$apply();
     }
+    
+    //Adds provided card to the specified collection. 
+    //Gives card a unique identifier property based on order of entry
+    c.add_to_collection = function(card, collection){
+        
+        
+        //Use current collection in the event that no collection has been specified
+        if (typeof collection === undefined){
+            c.collections[current_collection].cards.push(card);
+            
+        //Otherwise, push to specified collection
+        } else {
+            collection.cards.push(card);
+        }
+        //card.identifier = collection.length;
+        //console.log("Identifier: "+card.identifier);
+        //console.log(collection);
+        
+        
+    };
+    
+    //Creates a new JSON object for collections and adds it to the c.collections array
+    c.add_new_collection = function(name, cards){
+        console.log(cards);
+        //Add unique identifier to each card in the collection
+        for (var i = 0; i < cards.length; i++){
+            cards[i].identifier = i;
+        }
+        var x = {
+            "name": name,
+            "cards": cards,
+            "identifier": c.collections.length
+        };
+        
+        c.collections.push(x);
+        
+        return x;
+    };
+    
+    //Handles string for creating a new collection of cards from scratch
+    c.mass_import_collection = function(data, collection){
+    
+        //Turn string into array of card names
+        var entries = data.split(',');
+        
+        //Creates new collection if a name was given for one
+        /*if (typeof name !== "undefined"){
+            alert("New collection being made");
+            collection = c.add_new_collection(name, []);
+        }*/
 
-    
-    //c.search_for_card();
-    
-    //Retrieves 
-    function getCard(name){
+        //Import card objects using names
+        for (var i = 0; i < entries.length; i++){
+            entries[i] = entries[i].trim();
+            get_card_from_api(entries[i], collection);
+        }
+            
+           // card = c.process_card_data(hold);
+           
+
+         //console.log(c.collections);
+
+    };
+
+    //Retrieves card object from the API and adds it to the list of searche
+    function get_card_from_api(name, collection){
 	
 	url='https://www.ygohub.com/api/card_info?name='+name;
 	//console.log(url);
@@ -44,35 +128,16 @@ ygo_folder_app.controller('ygo_folder_controller', function ygo_folder_controlle
             crossDomain: true,
             contentType: 'application/x-www-form-urlencoded',
 
-            beforeSend:function(){
-                // this is where we append a loading image
-            },
             success: function(data){
-                var card = JSON.parse(data);
-                //console.log(card);
-                c.search_list.push(card.card);
+                var card = c.process_card_data(data);
+                collection.cards.push(card);
                 $scope.$apply();
-                
             },
+            
             error:function(jqXHR, textStatus, errorThrown){
                 console.log(textStatus);
             }
 	});
-    }
-    
-    /*function append_search_list(card){
-        c.search_list.push(card);
-    }*/
-
-    /*function displaySearch(card){
-
-        var txt=card.name;
-        var pic=card.thumbnail_path;
-        $("#results_name").text(txt);
-        $(".search_results_img").attr('src', pic);
-        //console.log('cardToSearch is being called');
-
-    }*/
-    
+    }    
 
 });
--- scripts/services.js
+++
@@ -1,5 +0,0 @@
-ygo_folder_app.service ('updateCollection', function(collection, item){
-    alert("BEEP");
-    //return collection.push(item);
-});
-    
